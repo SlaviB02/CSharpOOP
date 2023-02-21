148f0e0404e1bfdc76058892503dcfa1cf1bd403
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2023-02-21 19:52:53 +0200

    Add files via upload

---
+++ Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower,double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+
+       double DefaultFuelConsumption = 1.25;
+        public virtual double FuelConsumption { get { return DefaultFuelConsumption; } }
+        public double Fuel { get; set; }
+        public int HorsePower{get;set;}
+        public 	virtual void Drive(double kilometers)
+        {
+            Fuel -= FuelConsumption * kilometers;
+        }
+    }
+    public class Car:Vehicle
+    {
+        double DefaultFuelConsumption = 3;
+        public Car(int horsePower,double fuel):base(horsePower,fuel)
+        {
+
+        }
+        public override double FuelConsumption => DefaultFuelConsumption;
+
+    }
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+    public class SportCar:Car
+    {
+        double DefaultFuelConsumption = 10;
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+        public override double FuelConsumption => DefaultFuelConsumption;
+    }
+    public class Motorcycle:Vehicle
+    {
+        public Motorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+    public class RaceMotorcycle:Motorcycle
+    {
+        double DefaultFuelConsumption = 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+        public override double FuelConsumption => DefaultFuelConsumption;
+    }
+    public class CrossMotorcycle:Motorcycle
+    {
+        public CrossMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+}
---
+++ Inheritance/Restaurant/Restaurant.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string Name { get; set; }
+        public virtual decimal Price { get; set; }
+
+    }
+    public class Beverage:Product
+    {
+        public Beverage(string name,decimal price,double milliliters):base(name,price)
+        {
+            this.Milliliters = milliliters;
+        }
+        public virtual double  Milliliters { get; set; }
+    }
+    public class HotBeverage:Beverage
+    {
+        public HotBeverage(string name, decimal price, double milliliters) : base(name, price,milliliters)
+        {
+
+        }
+    }
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name,double caffeine) : base(name,0,0)
+        {
+            this.Caffeine = caffeine;
+        }
+       const double coffeeMilliliters = 50;
+       const decimal coffeePrice = 3.50M;
+        public override double Milliliters { get => coffeeMilliliters; }
+        public override decimal Price { get => coffeePrice; }
+        public double Caffeine{get;set;}
+    }
+    public class Tea:HotBeverage
+    {
+        public Tea(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+
+        }
+    }
+    public class ColdBeverage : Beverage
+    {
+        public ColdBeverage(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+
+        }
+    }
+
+    public class Food : Product
+    {
+        public Food(string name, decimal price, double grams) : base(name, price)
+        {
+            this.Grams = grams;
+        }
+        public virtual double Grams { get; set; }
+    }
+    public class Dessert:Food
+    {
+        public Dessert(string name,decimal price,double grams,double calories):base(name,price,grams)
+        {
+            this.Calories = calories;
+        }
+        public virtual double Calories { get; set; }
+    }
+    public class MainDish:Food
+    {
+        public MainDish(string name, decimal price, double grams) : base(name, price,grams)
+        {
+            
+        }
+    }
+    public class Starter : Food
+    {
+        public Starter(string name, decimal price, double grams) : base(name, price, grams)
+        {
+
+        }
+    }
+    public class Soup:Starter
+    {
+        public Soup(string name, decimal price, double grams) : base(name, price, grams)
+        {
+
+        }
+    }
+    public class Cake : Dessert
+    {
+        const double grams = 250;
+        public override double Grams { get => grams; }
+        const double calories = 1000;
+        public override double Calories { get =>calories; }
+         const decimal cakePrice = 5;
+
+        public override decimal Price { get => cakePrice; }
+        public Cake(string name) : base(name,0,0,0)
+        {
+            this.Name = name;
+        }
+
+    }
+    public class Fish:MainDish
+    {
+        const double grams = 22;
+        public override double Grams { get => grams; }
+        public Fish(string name, decimal price) : base(name, price, 0)
+        {
+
+        }
+    }
+
+
+}
