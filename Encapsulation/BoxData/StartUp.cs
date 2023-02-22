642126000860372a0ab4792b087bf088abc96636
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2023-02-22 18:04:51 +0200

    Add files via upload

---
+++ Encapsulation/BoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Box box = null;
+            try
+            {
+                box = new Box(length, width, height);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
+        }
+    }
+}
