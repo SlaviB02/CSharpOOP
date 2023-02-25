b6216fe560dcf66651a48f6151c0ae0bc4b0e7ac
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2023-02-25 23:47:26 +0200

    Add files via upload

---
+++ Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+   public class Circle:Shape
+    {
+        private double radius;
+        public double Radius { get; private set; }
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+        public override string Draw()
+        {
+            return base.Draw()+this.GetType().Name;
+        }
+    }
+
+}
